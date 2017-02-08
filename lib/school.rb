45a355145f565b2152bb67ccde18fc5fc7a2d883
Siobhan <siobhan.harley@gmail.com>
2017-02-08 16:51:25 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+class School
+  attr_reader :name, :roster, :grade
+
+  def initialize (name)
+  @name = name
+  @roster = {}
+  end
+
+  def add_student (name, grade)
+    roster[grade]||= []
+    roster[grade] << name
+  end
+
+  def grade (grade)
+    roster[grade]
+  end
+
+  def sort
+  sorted_hash = {}
+  roster.map do | k, v |
+    sorted_hash[k]||= []
+    sorted_hash[k] = v.sort
+   end
+   sorted_hash
+ end
+
+end
+#  roster.map do | k, v |
+  #    v.sort
+  #end
+  #  roster.sort.to_h
